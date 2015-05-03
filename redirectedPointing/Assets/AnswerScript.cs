57649d72200bac19ac0c0d3225558562b34d4085
Jad Khoury <jad-nicolas.khoury@epfl.ch>
2015-05-03 22:41:36 +0200

    answeers

---
+++ redirectedPointing/Assets/AnswerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerScript : MonoBehaviour {
+
+	private ControlScript control;
+	private GameObject hand;
+	private AnswerManager manager;
+
+	public void Awake ()
+	{
+		control = GameObject.FindGameObjectWithTag ("GameController").GetComponent<ControlScript> (); 
+		hand = control.triggerObject;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Init ()
+	{
+		this.collider.isTrigger = true;
+		manager = GetComponentInParent<AnswerManager> ();
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject == hand) {
+			manager.Answer(this.name);
+		}
+	}
+	
+}
---
+++ redirectedPointing/Assets/IIG/IK/AnswerManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerManager : MonoBehaviour
+{
+
+	public Material yesMaterial;
+	public Material noMaterial;
+	public GameObject answerPrefab;
+	private float circleRadius;
+	private ControlScript control;
+	private float answerRadius;
+	private GameObject[] buttons;
+
+	void Awake ()
+	{
+		control = GameObject.FindGameObjectWithTag ("GameController").GetComponent<ControlScript> ();
+		circleRadius = control.circleRadius;
+		answerRadius = 0.05f;
+		buttons = new GameObject[2];
+	}
+
+	public void Run ()
+	{
+		createAnswers ();
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void Answer (string s)
+	{
+		Debug.Log (s);
+		foreach (GameObject obj in buttons)
+			Object.Destroy (obj);
+	}
+
+	void createAnswers ()
+	{
+		for (int i = 0; i <2; ++i) {
+			float angle = i * 2 * Mathf.PI / 2;
+			float x = circleRadius * Mathf.Cos (angle);
+			float y = 0.021f; //dependant to the size of the mesh cube
+			float z = circleRadius * Mathf.Sin (angle);
+			GameObject answerClone = (GameObject)Instantiate (answerPrefab, transform.position + new Vector3 (x, y, z), Quaternion.identity);
+			answerClone.transform.parent = transform;
+			answerClone.transform.rotation = answerClone.transform.parent.rotation;
+			answerClone.transform.localScale = new Vector3 (2 * answerRadius, 2 * answerRadius, 0.005f); //because the scale rep the diameter not the radius
+			answerClone.renderer.material = (i == 0) ? noMaterial : yesMaterial;
+			answerClone.name = (i == 0) ? "NO" : "YES";
+			buttons [i] = answerClone;
+			answerClone.GetComponent<AnswerScript> ().Init ();
+		}
+
+	}
+}
